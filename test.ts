857bbc60ade071c433c2d83de4c7b103a3bde54f
shinpei_yoshihara <s-yoshihara@saitama-tec.co.jp>
2020-02-14 16:06:43 +0900

    the

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 // tests go here; this will not be compiled when this package is used as an extension.
 
-console.log("HEXXX");
-console.log(hex`11111`);
+//console.log("HEXXX");
+//console.log(hex`11111`);
 
